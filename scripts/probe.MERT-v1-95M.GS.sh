ae8f21bdec88059a6adf346003a369acc9850eb9
a43992899 <a43992899@gmail.com>
2025-05-29 20:42:50 +0800

    update qwen2audio gs config

--- scripts/probe.MERT-v1-95M.GS.sh
+++ scripts/probe.MERT-v1-95M.GS.sh
@@ -1,3 +1,3 @@
 rm -rf output/probe.GS.MERT-v1-95M
-python cli.py fit -c configs/probe.MERT-v1-95M.GS.yaml
-python cli.py test -c configs/probe.MERT-v1-95M.GS.yaml
+CUDA_VISIBLE_DEVICES=1 python cli.py fit -c configs/probe.MERT-v1-95M.GS.yaml
+CUDA_VISIBLE_DEVICES=1 python cli.py test -c configs/probe.MERT-v1-95M.GS.yaml
